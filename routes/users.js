8ab57e38175981c0c7ca0c3fedd1f971651b3181
praneetha2908 <praneethareddy2003@outlook.com>
2025-03-25 21:06:01 -0500

    added files

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
-let ratio=0.75;
-let acc=1000;
-router.get('/', (req, res, next) => {
+
+let ratio = 0.75;
+let acc = 1000;
+
+rrouter.get('/', (req, res, next) => {
   // Step 3: Modify acc by multiplying it with ratio
   acc *= ratio;
 
   // Step 4: Modify the response to display the result of acc
   res.send(`Value is: ${acc}`);
 });
+
+module.exports = router;
